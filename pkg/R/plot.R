8e2c60d5559213f26d3582bcfba2bf8c870f0156
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2012-10-22 13:31:57 +0000

    Updates to ciplot and grid evaluations.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nraia@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.R
+++ pkg/R/plot.R
@@ -11,3 +11,29 @@ plot.nls <- function(x, ...) {
         xyplot(y ~ x|f, df, type=c("g","p"), scales=list(x=list(relation="free")),
                xlab=NULL, ylab="Residuals", ...)
 }
+
+## check if the results of confint.nls have a class.  If so check the class or use a null S3 default and a method.
+cigrid <- function(ci, fun, npts=200) {
+  vals <- as.data.frame(
+    apply(ci, 1, function(v) seq(v[1],v[2],length.out=npts)))
+  ans <- array(apply(do.call(expand.grid,vals),1,fun),
+               c(npts,npts))
+  attr(ans, "vals") <- vals
+  class(ans) <- "cigrid"
+  ans
+}
+
+contourplot.cigrid <- function(x, data, ...) {
+  dots <- list(...)
+  dnms <- names(dots)
+  vals <- attr(x, "vals")
+  vnms <- names(vals)
+  if (!("xlab" %in% dnms)) dots$xlab <- vnms[1]
+  if (!("ylab" %in% dnms)) dots$ylab <- vnms[2]
+  dots$x <- unclass(x)
+  dots$row.values <- vals[[1]]
+  dots$column.values <- vals[[2]]
+  dots$xlim <- range(vals[[1]])
+  dots$ylim <- range(vals[[2]])
+  do.call(contourplot, dots)
+}
